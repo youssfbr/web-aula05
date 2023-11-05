9db89eee8a342f33ea41f196b1bda6a7dd3062b2
Alisson Youssf <youssfbr@gmail.com>
2023-11-05 00:53:43 -0300

    Seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_category (name) VALUES ('Eletronics');
+INSERT INTO tb_category (name) VALUES ('Books');
+
+INSERT INTO tb_product (name, price, category_id) VALUES ('TV Samsung 50" Smart', 2200.0, 1);
+INSERT INTO tb_product (name, price, category_id) VALUES ('Domain Driven Design', 120.0, 2);
+INSERT INTO tb_product (name, price, category_id) VALUES ('PS5', 4800.0, 1);
+INSERT INTO tb_product (name, price, category_id) VALUES ('Docker', 100.0, 2);

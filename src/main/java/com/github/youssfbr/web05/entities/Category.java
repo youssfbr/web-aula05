1db616092220546aa4abcd065d969d0d0ff9903c
Alisson Youssf <youssfbr@gmail.com>
2023-11-04 21:16:40 -0300

    Domain Model

---
+++ src/main/java/com/github/youssfbr/web05/entities/Category.java
@@ -0,0 +1,43 @@
+package com.github.youssfbr.web05.entities;
+
+import java.util.Objects;
+
+public class Category {
+    private Long id;
+    private String name;
+
+    public Category() { }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(getId(), category.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
---
+++ src/main/java/com/github/youssfbr/web05/entities/Product.java
@@ -0,0 +1,63 @@
+package com.github.youssfbr.web05.entities;
+
+import java.util.Objects;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+    private Category category;
+
+    public Product() { }
+
+    public Product(Long id, String name, Double price, Category category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product product)) return false;
+        return Objects.equals(getId(), product.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}

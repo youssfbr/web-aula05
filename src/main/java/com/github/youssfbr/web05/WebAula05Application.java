cf585c1344bd72daaa630a896065516d15a27fd2
Alisson Youssf <youssfbr@gmail.com>
2023-11-04 21:06:30 -0300

    Project created

---
+++ src/main/java/com/github/youssfbr/web05/WebAula05Application.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.web05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAula05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAula05Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/youssfbr/web05/WebAula05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.web05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAula05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
